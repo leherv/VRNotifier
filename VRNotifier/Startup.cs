b58de07b9a7002b8ebca5d182e6fc88172fc5b15
Viktor Leher <viktor.leher@gmail.com>
2021-01-15 16:05:31 +0100

    added file logging for debugging

--- VRNotifier/Startup.cs
+++ VRNotifier/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using VRNotifier.Config;
 using VRNotifier.Services;
 using VRPersistence.Config;
@@ -45,8 +46,9 @@ namespace VRNotifier
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile("Logs/vrnotifier-{Date}.txt");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
